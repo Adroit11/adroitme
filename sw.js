88d1bc1bf96d2aeb7e222ea3001249563fed663d
Ogundiji Bolade Adio <kamponistullar@gmail.com>
2018-06-07 18:45:42 +0100

    sw: caching images https error fixed

--- sw.js
+++ sw.js
@@ -31,19 +31,7 @@ var URLS = [                            // Add URL you want to cache in this lis
   '/adroitme/src/js/app.js',
   'https://res.cloudinary.com/dc9kfp5gt/image/upload/c_scale,e_art:incognito,h_340,q_100,w_330/v1519764764/IMG_20180227_214951_697_o3buej.jpg',
   'https://res.cloudinary.com/dc9kfp5gt/image/upload/q_100/v1528384527/screencapture-localhost-3000-2018-06-05-20_41_26_rbrhy9.png',
-  'https://res.cloudinary.com/dc9kfp5gt/image/upload/q_100/v1528384519/screencapture-localhost-alc-cryptoproject2-2018-05-29-18_01_21_hvewgy.png',
-  'https://res.cloudinary.com/dc9kfp5gt/image/upload/q_100/v1519847220/screencapture-localhost-AdroitCodeEditor-1519847116464_w1fkwy.png',
-  'https://res.cloudinary.com/dc9kfp5gt/image/upload/q_100/v1519841760/IMG_20171221_095641_175_yjf3hk.jpg',
-  'https://res.cloudinary.com/dc9kfp5gt/image/upload/c_scale,h_997,q_100,w_2282/v1519844075/IMG_20180228_195245_856_q2rmgp.jpg',
-  'https://res.cloudinary.com/dc9kfp5gt/image/upload/q_100/v1519844075/IMG_20180228_195206_491_pbfxhk.jpg',
-  'https://res.cloudinary.com/dc9kfp5gt/image/upload/q_100/v1519844075/IMG_20180228_195119_365_h0qqlb.jpg',
-  'cloudinary.com/dc9kfp5gt/image/upload/q_100/v1519844074/IMG_20180228_195319_319_oav1q3.jpg',
-  'https://res.cloudinary.com/dc9kfp5gt/image/upload/q_100/v1519841760/IMG_20171228_080715_466_iiosgv.jpg',
-  'https://res.cloudinary.com/dc9kfp5gt/image/upload/q_100/v1519844074/IMG_20180228_195029_290_zznqvs.jpg',
-  'https://res.cloudinary.com/dc9kfp5gt/image/upload/q_100/v1519841760/IMG_20171025_085240_684_uqhbxq.jpg',
-  'https://res.cloudinary.com/dc9kfp5gt/image/upload/c_scale,h_400,q_100,w_460/v1519840117/Google-Dev-EMEA-Badge_tahpnu.png',
-  'https://res.cloudinary.com/dc9kfp5gt/image/upload/c_scale,h_400,q_100,w_460/v1519835751/DHUUexfXUAAnXn3_ahhhpp.jpg',
-  'https://res.cloudinary.com/dc9kfp5gt/image/upload/q_100/v1520243603/left_bg_1_umblkn.png'    
+  'https://res.cloudinary.com/dc9kfp5gt/image/upload/q_100/v1528384519/screencapture-localhost-alc-cryptoproject2-2018-05-29-18_01_21_hvewgy.png'    
 ]
 
 // Respond with cached resources
